583efb71b788ada2ff5a272bc60712bf25b1bc0b
sowjanya <30652382+sowjanya467@users.noreply.github.com>
2024-05-18 21:24:03 +0530

    Update CodeScanner.py

--- CodeScanner.py
+++ CodeScanner.py
@@ -1,10 +1,11 @@
+
 import requests
 import json
 import os
 
 # Fetch GitHub Personal Access Token from environment variable
-TOKEN = os.getenv("TOKEN")
-
+#TOKEN = os.getenv("TOKEN")
+print("TOKEN:"),
 params = {'per_page': 100}
 alerts = []
 if not TOKEN:
@@ -18,12 +19,14 @@ def fetch_code_scanning_alerts(token):
         "Authorization": f"token {token}",
         #        "Accept": "application/vnd.github+json",
     }
+    #url = f"https://api.github.com/orgs/{org}/code-scanning/alerts"
     url = f"https://api.github.com/repos/sowjanya467/user_microservice/code-scanning/alerts"
-  
+
     page = 1
     try:
         while True:
             response = requests.get(url, headers=headers, params=params)
+            print(response)
             response_json = response.json()
             if response.status_code == 200:
                 alerts.extend(response_json)
@@ -44,7 +47,7 @@ def fetch_code_scanning_alerts(token):
 def main():
     alert_data = fetch_code_scanning_alerts(TOKEN)
     if alert_data:
-        with open('code_scanning_alerts.json', 'w') as f:
+        with open('code-scanning-alerts.json', 'w') as f:
             json.dump(alert_data, f, indent=4)
     else:
         print("Failed to fetch code-scanning alerts data or alerts data is empty.")
