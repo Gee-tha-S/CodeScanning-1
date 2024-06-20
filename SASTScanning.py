e828458edf65179a05ff42abde6f09fbc70945f4
sowjanya <30652382+sowjanya467@users.noreply.github.com>
2024-06-20 17:02:33 +0530

    Update SASTScanning.py

--- SASTScanning.py
+++ SASTScanning.py
@@ -12,19 +12,7 @@ with open(json_file_path, 'r') as f:
 df = json_normalize(data)
 
 # Define new data for additional columns
-repodf = pd.DataFrame(data, columns = ['repository.name'])
-column_data=[]
-eaiNumber=[]
-column_data=df['repository.name'].tolist()
-for reponame in column_data: 
-    eaiData=reponame.split("-")
-    eaiNumber.append(eaiData[1])
-    #print(eaiNumber)
 
-
-# Add multiple columns using dictionary assignment
-new_data = {'EAI Number': eaiNumber}
-df = df.assign(**new_data)
 
 #print(df)
 ##############################################################################
