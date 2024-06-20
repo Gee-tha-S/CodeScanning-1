d981eb9bbada8a9b832cc6b7ad2d13e1483bf8c3
sowjanya <30652382+sowjanya467@users.noreply.github.com>
2024-06-20 16:57:35 +0530

    Update SASTScanning.py

--- SASTScanning.py
+++ SASTScanning.py
@@ -1,29 +1,35 @@
-import requests
-import json
-import os
- 
-token = os.getenv("TOKEN")
- 
-headers = {
-    'Authorization': f'token {token}',
-    'Accept': 'application/vnd.github.v3+json'
-}
- 
-repos_url = 'https://api.github.com/sowjanya467/repos'
-response = requests.get(repos_url, headers=headers)
-repos = response.json()
- 
-alerts = []
-for repo in repos:
-    # Fetch code scanning alerts for each repository in github
-    alerts_url = f"https://api.github.com/repos/{repo}/code-scanning/alerts"
-    response = requests.get(alerts_url, headers=headers)
-    repo_alerts = response.json()
-    
-    alerts.extend(repo_alerts)
- 
 
-with open('scanning_alerts.json', 'w') as file:
-    json.dump(alerts, file)
+import pandas as pd
+from pandas import json_normalize
+import json
  
-print("Code scanning alerts saved to code_scanning_alerts.json")
+# Load the JSON file
+json_file_path = 'fedex.json'
+with open(json_file_path, 'r') as f:
+    data = json.load(f)
+ 
+# Normalize the nested JSON
+df = json_normalize(data)
+
+# Define new data for additional columns
+repodf = pd.DataFrame(data, columns = ['repository.name'])
+column_data=[]
+eaiNumber=[]
+column_data=df['repository.name'].tolist()
+for reponame in column_data: 
+    eaiData=reponame.split("-")
+    eaiNumber.append(eaiData[1])
+    #print(eaiNumber)
+
+
+# Add multiple columns using dictionary assignment
+new_data = {'EAI Number': eaiNumber}
+df = df.assign(**new_data)
+
+#print(df)
+##############################################################################
+# Convert the DataFrame to a CSV file
+csv_file_path = 'code-scanning-results.csv'
+df.to_csv(csv_file_path, index=False)
+ 
+print(f"Nested JSON file has been converted to CSV file at: {csv_file_path}")
